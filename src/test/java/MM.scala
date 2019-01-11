a65501c357222c46ce63cbc4da731195057dcee0
zing <x1zing@126.com>
2019-01-12 00:30:17 +0800

    test

--- src/test/java/MM.scala
+++ src/test/java/MM.scala
@@ -1,14 +1,14 @@
-import com.zing.utils.ByteUtil._
+import com.zing.utils.HexUtil._
 
 
 object MM {
   def main(args: Array[String]) = {
     val da: Array[Byte] = Array(1, 2, 3, 4,15,16);
     val s = toHex(da)
-    val b = hexStrToBytes(s)
+    val b = toBytes(s)
 
     println(s)
-    println(hexStrToBytes("Dd102a40F10").mkString)
+    println(toBytes("Dd102a40F10").mkString)
   }
 
 
---
+++ src/test/java/StringTests.scala
@@ -0,0 +1,47 @@
+import com.zing.utils.HexUtil._
+
+object StringTests {
+  //  split
+  def main(args: Array[String]): Unit = {
+    val str: String = "monster"
+    str.foreach(s => print(s + " "))
+    str.foreach(println)
+
+    println(str.charAt(3))
+
+    println(str.filter(_ != 'm'))
+
+    println(toHex("0".getBytes()))
+
+    println(str.drop(2).take(2))
+
+    println(str.capitalize)
+
+    println(str)
+
+    //true
+    println(str == 'm' + "onster")
+    // true
+    println(str == "monster")
+
+    //查找
+    println(str.filter(_ > 's').groupBy(c => c.toByte).keys.mkString(","))
+
+    "\\d+".r.findFirstIn("12dsafnsjk12") match {
+      case Some(n) => println(n)
+      case _ => println("nothing find")
+    }
+
+    "\\d".r.findAllIn("92dsafnsjk 121sd 523").foreach(f => println(f))
+
+    //替换
+    println("fuck".r.replaceAllIn("fuck U U fucker", "****"))
+    println("hate".r.replaceFirstIn("I hate U but I like her", "love"))
+
+
+
+
+
+  }
+
+}
