307a41f23861232103bd7e787c23dd1e4195f405
zing <x1zing@126.com>
2019-01-16 11:25:12 +0800

    number Test

---
+++ src/test/java/NumberTests.scala
@@ -0,0 +1,57 @@
+import scala.collection.immutable.StringLike
+import scala.language.implicitConversions
+
+
+object NumberTests {
+
+  implicit def int2Str(a: Int): String = {
+    a.toString
+  }
+
+  def main(args: Array[String]): Unit = {
+
+    println(1 + 2)
+    println(1 + "2")
+    println("1" + "2")
+
+
+    plus(1, 2);
+    println(plus(1, 2))
+
+
+    println(plus("1", "2"))
+  }
+
+
+  def plus(a: Int, b: Int) = {
+    a + b
+  }
+
+  def plus(a: Option[Int], b: Option[Int]) = {
+    a match {
+      case Some(m) => b match {
+        case Some(n) => m + n
+        case _ => 0
+      }
+      case _ => 0
+    }
+  }
+
+
+  //def plus(a: String, b: String): String = {
+  //  a + b;
+  //}
+
+  implicit def toInt(s: String): Option[Int] = {
+    try {
+      Some(s.toInt)
+    } catch {
+      case e: Exception => {
+        e.printStackTrace()
+        None
+      }
+    }
+  }
+
+
+}
