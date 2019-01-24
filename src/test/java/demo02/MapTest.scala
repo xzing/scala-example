2360b3f470ad2cc6971d7c9ef73a3c80601a7b5c
zing <x1zing@126.com>
2019-01-24 20:40:16 +0800

    Map Test

--- src/test/java/demo02/MapTest.scala
+++ src/test/java/demo02/MapTest.scala
@@ -1,5 +1,7 @@
 package demo02
 
+import scala.collection.SortedMap
+
 object MapTest {
   def main(args: Array[String]): Unit = {
     val aMap: Map[Int, String] = Map(1 -> "sd", 2 -> "250", 3 -> "love")
@@ -14,7 +16,32 @@ object MapTest {
     println("----------")
     aMap.keys.map(println(_))
 
+    val s = SortedMap(3 -> "喵", 2 -> "😡", 6 -> "")
+    s.mapValues(println(_))
+    s.keys.map(println(_))
+    s.values.map(println(_))
 
+    val immutableMap = scala.collection.immutable.ListMap(3 -> "喵", 2 -> "😡", 6 -> "")
+    immutableMap.mapValues(println(_))
+    immutableMap.keys.map(println(_))
+    immutableMap.values.map(println(_))
+    println("----------")
+    immutableMap.updated(4, "samian")
+    immutableMap.mapValues(println(_))
+    immutableMap.keys.map(println(_))
+    immutableMap.values.map(println(_))
+
+
+    println("----------")
+    val mutableMap = scala.collection.mutable.ListMap(3 -> "喵", 2 -> "😡", 6 -> "")
+    mutableMap.mapValues(println(_))
+    mutableMap.keys.map(println(_))
+    mutableMap.values.map(println(_))
+    println("----------")
+    mutableMap.update(3, "sa")
+    mutableMap.mapValues(println(_))
+    mutableMap.keys.map(println(_))
+    mutableMap.values.map(println(_))
   }
 
 }
