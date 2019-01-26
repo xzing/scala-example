27c5472c9003eef065545c03266913009fc4ec4e
zing <x1zing@126.com>
2019-01-26 22:52:12 +0800

    Map Test

--- src/test/java/demo02/MethodTest.scala
+++ src/test/java/demo02/MethodTest.scala
@@ -1,5 +1,24 @@
 package demo02
 
+
 object MethodTest {
 
+  def main(args: Array[String]): Unit = {
+    Option.apply("23333") match {
+      case Some(h) => println(h)
+      case _ => println("null")
+    }
+
+
+    Option.apply(null) match {
+      case Some(h) => println(h)
+      case _ => println("null")
+    }
+
+    printAll("a", "b", "ahaha")
+  }
+
+  def printAll(s: String*) = {
+    s.foreach(println)
+  }
 }
