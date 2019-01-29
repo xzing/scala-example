639a2e913b6565efb682f24c2a7445e6adac4605
zing <x1zing@126.com>
2019-01-29 18:13:33 +0800

    Class Test

--- src/test/java/demo02/ClassAndObjectTest.scala
+++ src/test/java/demo02/ClassAndObjectTest.scala
@@ -12,7 +12,15 @@ object ClassAndObjectTest {
     }
   }
 
-  class Test03(private var name: String) {
+  /**
+    * private 私有化构造函数
+    * 可以用等号设置默认值
+    *
+    * @param name
+    */
+  class Test03 private(var name: String, var s: String = "defaultValue") {
+    //辅助构造函数只能用this
+
     def this() {
       this("no")
     }
@@ -20,6 +28,12 @@ object ClassAndObjectTest {
 
     def canEqual(other: Any): Boolean = other.isInstanceOf[Test03]
 
+    /**
+      * 覆写
+      *
+      * @param other
+      * @return
+      */
     override def equals(other: Any): Boolean = other match {
       case that: Test03 =>
         (that canEqual this) &&
@@ -33,6 +47,14 @@ object ClassAndObjectTest {
     }
   }
 
+
+  class Test04() {
+    /**
+      * 阻止m生成 getter 和 setter
+      */
+    var m: String = _
+  }
+
   def main(args: Array[String]): Unit = {
     val t1 = new Test01("2333")
     val t2 = new Test02("2333")
@@ -43,6 +65,8 @@ object ClassAndObjectTest {
     println(s"t1.name = ${t2}")
     //println(s"t1.name = ${t2.name}") // error
     println(s"t1.name = ${t2.getName}")
+
+    println()
   }
 
 }
